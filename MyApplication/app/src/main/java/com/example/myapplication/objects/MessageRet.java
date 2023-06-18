453bba9a8af0aece704f459f8040959674fef46d
Tal Achizvi <118102450+tachizvi@users.noreply.github.com>
2023-06-18 18:57:34 +0300

    Adding all the server API in the android

---
+++ MyApplication/app/src/main/java/com/example/myapplication/objects/MessageRet.java
@@ -0,0 +1,47 @@
+package com.example.myapplication.objects;
+
+public class MessageRet {
+    private int id;
+    private String created;
+    private Sender sender;
+    private String content;
+
+    public MessageRet(int id, String created, Sender sender, String content) {
+        this.id = id;
+        this.created = created;
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public Sender getSender() {
+        return sender;
+    }
+
+    public void setSender(Sender sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
